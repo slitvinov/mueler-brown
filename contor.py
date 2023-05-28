bc97a68882da54a72545fbf1aac8e88b06fb0afe
lisergey <lisergey@penguin>
2023-05-27 22:00:08 -0400

    up

---
+++ contor.py
@@ -0,0 +1,35 @@
+import matplotlib.pylab as plt
+import numpy as np
+import mueler_brown
+
+
+def sqspace(a, b, n):
+    s = (b - a)**0.5
+    x = np.linspace(0, s, n)**2
+    return x + a
+
+
+xlo, xhi = -1.5, 1.5
+ylo, yhi = -1.0, 2.0
+n = 200
+xrange = np.linspace(xlo, xhi, n)
+yrange = np.linspace(ylo, yhi, n)
+
+Emin0 = -1.4669951720995402e+02
+Emin1 = -1.0816672411685234e+02
+Emin2 = -8.0767818129659005e+01
+kT = 15
+
+E = [[mueler_brown.energy(x, y) for x in xrange] for y in yrange]
+
+levels0 = sqspace(Emin0 + kT / 50, Emin0 + 3 * kT, 4)
+levels1 = sqspace(Emin1 + kT / 50, Emin1 + 3 * kT, 4)
+levels2 = sqspace(Emin2 + kT / 50, Emin2 + 3 * kT, 4)
+levels = np.sort(np.hstack((levels0, levels1, levels2)))
+
+plt.xlim(xlo, xhi)
+plt.ylim(ylo, yhi)
+plt.contour(xrange, yrange, E, levels=levels0, colors='r', linestyles='solid')
+plt.contour(xrange, yrange, E, levels=levels1, colors='g', linestyles='solid')
+plt.contour(xrange, yrange, E, levels=levels2, colors='b', linestyles='solid')
+plt.show()
--- states.py
+++ states.py
@@ -1,15 +1,24 @@
 import matplotlib.pylab as plt
 import numpy as np
 import mueler_brown
-import mmap
+import sys
 
+argv = sys.argv[:]
+argv.pop(0)
 dtype = np.dtype("<f8, <i4", align=True)
-n = 1_000_000
-buf = bytearray(n * dtype.itemsize)
-mueler_brown.states(n, buf)
+if len(argv) == 0:
+    n = 10_000_000
+    buf = bytearray(n * dtype.itemsize)
+    mueler_brown.states(n, buf)
+    with open("out.bin", "wb") as f:
+        f.write(buf)
+else:
+    with open(argv[0], "rb") as f:
+        buf = f.read()
+    n = len(buf) // dtype.itemsize
 S = np.ndarray(n, dtype, buffer=buf, order='F')
 T01 = S["f0"][S["f1"] == 1]
 T10 = S["f0"][S["f1"] == 0]
-plt.hist(np.log(T01), alpha=0.5, bins=100, density=True)
-plt.hist(np.log(T10), alpha=0.5, bins=100, density=True)
+plt.hist(np.log10(T01), alpha=0.5, bins=50, density=True)
+plt.hist(np.log10(T10), alpha=0.5, bins=50, density=True)
 plt.savefig("states.png")
